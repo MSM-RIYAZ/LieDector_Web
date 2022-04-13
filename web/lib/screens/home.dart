e736be9fce96f42b30503b3f35aa0434ce1822b1
Fathima Sineera <ict1700131@sjp.ac.lk>
2022-04-13 16:42:43 +0530

    main contaner wrapped with expanted

--- web/lib/screens/home.dart
+++ web/lib/screens/home.dart
@@ -20,64 +20,66 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          decoration:BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("images/bg_eye.png"),
-              fit: BoxFit.cover,),
-            
-          ),
-          child: Column(
-            children:[
-              Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:[
-                    Image(image:AssetImage('images/camera.png'),),
-                    Text(
-                      'Lie Detection System',
-                      style:GoogleFonts.roboto(
-                        color: colGreen,
-                        fontSize: 60.0,
-                        fontWeight: FontWeight.bold,
-                      ))],
+        child: Expanded(
+          child: Container(
+            decoration:BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/bg_eye.png"),
+                fit: BoxFit.cover,),
+              
+            ),
+            child: Column(
+              children:[
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      Image(image:AssetImage('images/camera.png'),),
+                      Text(
+                        'Lie Detection System',
+                        style:GoogleFonts.roboto(
+                          color: colGreen,
+                          fontSize: 60.0,
+                          fontWeight: FontWeight.bold,
+                        ))],
+                  ),
                 ),
+                Expanded(
+                  flex: 3,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        color: colGreen,
+                        height: 500,
+                        width: double.infinity,
+                      ),
+        
+                      Container(color: colBodyGrey,),
+                    ],
+                  )),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      Text(
+                        bullet+ 'AAAAAAA',
+                        style: TextStyle(color: colBodyGrey)),
+                      Text(
+                        bullet+ 'BBBBBBB',
+                        style: TextStyle(color: colBodyGrey)),
+                      Text(
+                        bullet+ 'CCCCCCC',
+                        style: TextStyle(color: colBodyGrey)),
+                    
+                    ],
+                  )),
+              ],
+        
+            ),
               ),
-              Expanded(
-                flex: 3,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: colGreen,
-                      height: 500,
-                      width: double.infinity,
-                    ),
-
-                    Container(color: colBodyGrey,),
-                  ],
-                )),
-              Expanded(
-                flex: 2,
-                child: Column(
-                  children: [
-                    Text(
-                      bullet+ 'AAAAAAA',
-                      style: TextStyle(color: colBodyGrey)),
-                    Text(
-                      bullet+ 'BBBBBBB',
-                      style: TextStyle(color: colBodyGrey)),
-                    Text(
-                      bullet+ 'CCCCCCC',
-                      style: TextStyle(color: colBodyGrey)),
-                  
-                  ],
-                )),
-            ],
-
-          ),
-      ),),
+        ),),
     );
   }
 }
