71599ea3ef1722a53b3efeb4f6d7e0751bb25fa3
Fathima Sineera <ict1700131@sjp.ac.lk>
2022-06-26 12:57:17 +0530

    home screen changed

--- web/lib/screens/home.dart
+++ web/lib/screens/home.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors, deprecated_member_use
-
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import '../helper/style.dart';
+import '../widgets/body.dart';
+import '../widgets/footer.dart';
+import '../widgets/header.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,88 +12,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Color colBodyGrey =Color(0xFF7F7C82);
-  Color colWhite =Color(0xFFF3F1F5);
-  Color colGreen =Color(0xFF007600);
-  String bullet = "\u2022 ";
-
   @override
   Widget build(BuildContext context) {
-    double width_ = MediaQuery.of(context).size.width;
-    double height_ = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
         child: Expanded(
           child: Container(
-            height: height_,
-            width: width_,
-            decoration:BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("images/bg_eye.png"),
-                fit: BoxFit.cover,),
-              
-            ),
+            decoration: bgDecoration,
             child: Column(
-              children:[
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children:[
-                      Image(image:AssetImage('images/camera.png'),),
-                      Text(
-                        'Lie Detection System',
-                        style:GoogleFonts.roboto(
-                          color: colGreen,
-                          fontSize: 60.0,
-                          fontWeight: FontWeight.bold,
-                        ))],
-                  ),
+              children: [
+                const Header(
+                    image: "images/camera.png", text: "Lie DetectionSystem"),
+                Body(
+                  processingOnTap: () {},
                 ),
-                Expanded(
-                  flex: 3,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          color: colGreen,
-                          height: 100,
-                          width: 100,
-                        ),
-                      ),
-        
-                      Expanded(
-                        flex: 1,
-                        child: Container(color: colBodyGrey,height: 100,
-                          width: 100,
-                        
-                        ),
-                      ),
-                    ],
-                  )),
-                Expanded(
-                  flex: 2,
-                  child: Column(
-                    children: [
-                      Text(
-                        bullet+ 'AAAAAAA',
-                        style: TextStyle(color: colBodyGrey)),
-                      Text(
-                        bullet+ 'BBBBBBB',
-                        style: TextStyle(color: colBodyGrey)),
-                      Text(
-                        bullet+ 'CCCCCCC',
-                        style: TextStyle(color: colBodyGrey)),
-                    
-                    ],
-                  )),
+                const Footer(
+                  instructionText1: instruction1,
+                  instructionText2: instruction2,
+                  instructionText3: instruction3,
+                ),
               ],
-        
             ),
-              ),
-        ),),
+          ),
+        ),
+      ),
     );
   }
 }
--- web/test/widget_test.dart
+++ web/test/widget_test.dart
@@ -5,11 +5,11 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+//import 'package:flutter/material.dart';
+//import 'package:flutter_test/flutter_test.dart';
 
-import 'package:web/main.dart';
-
+//import 'package:web/main.dart';
+/*
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
@@ -27,4 +27,4 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
-}
+}*/
